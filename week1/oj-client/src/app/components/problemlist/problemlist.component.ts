f8c319b056dc9a55fa679dd4a158ceaa4bba26c5
xlal1234 <526294596@qq.com>
2020-02-10 10:18:51 +0800

    fix problem UpperCase

--- week1/oj-client/src/app/components/problemlist/problemlist.component.ts
+++ week1/oj-client/src/app/components/problemlist/problemlist.component.ts
@@ -6,31 +6,31 @@ export const PROBLEMS: Problem[] = [
     id: 1,
     name: "Two Sum",
     desc: 'Given an array of integers, return indices of the two numbers such that they add up to a specific target.',
-    difficulty: "easy"
+    difficulty: "EASY"
   },
   {
     id: 2,
     name: "Three Sum",
     desc: 'Given an array S of integers, find three numbers..',
-    difficulty: "medium"
+    difficulty: "MEDIUM"
   },
   {
     id: 3,
     name: "4Sum",
     desc: 'Given an array S of integers, find three numbers..',
-    difficulty: "medium"
+    difficulty: "MEDIUM"
   },
   {
     id: 4,
     name: "Triangle Count",
     desc: 'Given an array S of integers, find three numbers..',
-    difficulty: "hard"
+    difficulty: "Hard"
   },
   {
     id: 5,
     name: "Sliding Window Maximum",
     desc: 'Given an array S of integers, find three numbers..',
-    difficulty: "hard"
+    difficulty: "Hard"
   }
 ];
 
